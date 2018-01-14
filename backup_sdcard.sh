ae16ec885ae9703e59017a542853f360596ac95d
Alex Gaudio <adgaudio@gmail.com>
2018-01-14 18:14:15 -0500

    back sd card script

---
+++ backup_sdcard.sh
@@ -0,0 +1,28 @@
+# backup and compress a device file (like external SD card)
+
+set -e
+set -u
+set -x
+
+function main {
+local device="${1}"
+local img="${2:-/tmp/`date +%Y%m%d`-sd.img}"
+
+sudo umount $device* || /bin/true
+sudo dcfldd if=$device  | pigz  > ${img}.gz
+sudo sync
+
+# sudo dcfldd if=$device of=${img}.large
+# sudo sync
+# ./PiShrink/pishrink.sh ${img}.large $img
+# rm ${img}.large
+# pigz ${img}
+
+ls -sh $img.gz
+}
+
+if [[ "${1:--h}" = "-h" ]] ; then
+  echo "usage:  $0  /dev/device-file  [output-file.img]"
+  exit 1
+fi
+main $@
