ff88920b4740d4db6be9ffc376ca8f42756241a5
Alex Gaudio <adgaudio@gmail.com>
2018-03-10 00:43:54 -0500

    shrink image by default.  fix pishrink to latest version.  uses root.

--- backup_sdcard.sh
+++ backup_sdcard.sh
@@ -8,15 +8,17 @@ function main {
 local device="${1}"
 local img="${2:-/tmp/`date +%Y%m%d`-sd.img}"
 
-sudo umount $device* || /bin/true
-sudo dcfldd if=$device  | pigz  > ${img}.gz
-sudo sync
-
-# sudo dcfldd if=$device of=${img}.large
+# sudo umount $device* || /bin/true
+# sudo dcfldd if=$device  | pigz  > ${img}.gz
 # sudo sync
-# ./PiShrink/pishrink.sh ${img}.large $img
-# rm ${img}.large
-# pigz ${img}
+
+sudo -s -- <<EOF
+dcfldd if=$device of=${img}.large
+sync
+./PiShrink/pishrink.sh ${img}.large $img
+EOF
+rm -f ${img}.large
+pigz ${img}
 
 ls -sh $img.gz
 }
